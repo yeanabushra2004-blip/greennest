8f7b1ee4bf92a5d71762be6af9e573329179e49e
YEANA BUSHRA <yeanabushra2004@gmail.com>
2026-07-24 23:09:50 +0600

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,9 @@
+document.getElementById("contactForm").addEventListener("submit",function(e){
+
+e.preventDefault();
+
+alert("Thank you for contacting GreenNest! We will get back to you soon.");
+
+this.reset();
+
+});
